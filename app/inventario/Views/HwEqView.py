d26657f794b288dc36f57b599df5629b838eafff
usforty <usfortydep@gmail.com>
2020-02-04 12:53:12 -0500

    04/02

--- app/inventario/Views/HwEqView.py
+++ app/inventario/Views/HwEqView.py
@@ -7,26 +7,19 @@ from inventario.models import Equipo, Area
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.contrib import messages
-
-
-def AreaData():
-    queryset = Area.objects.all()
-    return queryset
-
+from .viewAreaData import AreaData, MarcaData
 
 def EquipoIndex(request):
-    return render(request, 'Hardware/HardwareEquipo/EC.html', {'queryarea': AreaData()})
-
+    return render(request, 'Hardware/HardwareEquipo/EC.html', {'queryarea': AreaData(), 'querynombre':MarcaData(0)})
 
 def BEquipoIndex(request):
-    return render(request, 'Hardware/HardwareEquipo/ER.html', {'queryarea': AreaData()})
+    return render(request, 'Hardware/HardwareEquipo/ER.html', {'queryarea': AreaData(), 'querynombre':MarcaData(0)})
 
 
 def EquipoDniEmpleado(request, dniempleado):
     query = Equipo.objects.filter(dni_empleado=dniempleado)
     querys = EquipoSerializer(query, many=True)
     return JsonResponse(querys.data, safe=False)
-
 
 @api_view(['GET', 'POST'])
 def EquipoGetAllCreate(request):
@@ -35,7 +28,6 @@ def EquipoGetAllCreate(request):
         equiposerializer = EquipoSerializer(equipo, many=True)
         return Response(equiposerializer.data)
     elif request.method == 'POST':
-        print(request.data)
         equiposerializer = EquipoSerializer(data=request.data)
         if equiposerializer.is_valid():
             equiposerializer.save()
@@ -57,12 +49,13 @@ def EquipoGetUpdate(request, idequipo):
         return JsonResponse({"error": 404}, status=404)
     if request.method == 'GET':
         area = equipo.dni_empleado.id_area
+        marca=equipo.marca_equipo.nombre_marca
         sarea = AreaSerializer(area, many=False)
         empleado = equipo.dni_empleado
         sempleado = EmpleadoSerializer(empleado, many=False)
         serializer = EquipoSerializer(equipo)
-        querys = serializer.data
-        return render(request, 'Hardware/HardwareEquipo/EU.html', {'querys': serializer.data, 'queryarea': AreaData(), 'area': sarea.data, 'empleado': sempleado.data})
+        print(serializer.data)
+        return render(request, 'Hardware/HardwareEquipo/EU.html', {'querys': serializer.data, 'querymarca':marca, 'querynombre':MarcaData(0), 'queryarea': AreaData(), 'area': sarea.data, 'empleado': sempleado.data})
     elif request.method == 'POST':
         print("estas en post")
         serializer = EquipoSerializer(equipo, data=request.data)
